490dc472ca81096c719e0907d3a7fec4d27bd040
matheuspdf <96511087+matheuspdf@users.noreply.github.com>
2022-02-28 15:08:25 -0300

    Create script.js

---
+++ aulas/escopo lexico/script.js
@@ -0,0 +1,8 @@
+const nome = 'matheus gabriel silva lopes'
+
+function falaNome() {
+  console.log(nome)
+}
+falaNome()
+
+// O trecho do código em que uma variável qualquer é identificada por um nome é definido como o escopo léxico da variável. ... Fora desse bloco, a variável é indefinida ou um outro valor é identificado por esse nome.
