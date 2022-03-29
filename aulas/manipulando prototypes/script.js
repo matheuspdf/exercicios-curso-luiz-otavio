de0e0f3e65c33039bf1e4eed926a1c28a330cf70
matheuspdf <96511087+matheuspdf@users.noreply.github.com>
2022-03-29 20:01:05 -0300

    script.js

---
+++ aulas/manipulando prototypes/script.js
@@ -0,0 +1,16 @@
+const objA = {
+  chaveA: 'A'
+}
+
+const objB = {
+  chaveB: 'B'
+}
+
+const objC = new Object()
+objC.chaveC = 'C'
+
+Object.setPrototypeOf(objB, objA)
+
+Object.setPrototypeOf(objC, objB)
+
+console.log(objC, chaveA)
